57158482aa0cb92615b507076078b8a9dfef0ce4
Govind Tank <govindtank600@gmail.com>
2026-05-29 21:33:33 +0530

    blog: Add The Evolution of Kotlin Multiplatform in 2026 - generated on 2026-05-29

---
+++ dist/assets/stateDiagram-v2-BHNVJYJU-YTDkn86Z.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-AQP2D5EJ-CuzBl00b.js";import{_ as i}from"./index-x-xE82pt.js";import"./chunk-55IACEB6-BxSalOBy.js";import"./chunk-2J33WTMH-DUUkOXXq.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
