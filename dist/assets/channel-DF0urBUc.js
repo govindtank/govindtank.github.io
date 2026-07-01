d9bf37a1c440472cf85436f9743a50cb79bf109d
Govind Tank <govindtank600@gmail.com>
2026-07-01 08:06:03 +0530

    blog: Add FinOps for Kubernetes: Optimizing Cloud Costs in Containerized Environments - 2026-07-01
    Automated via Qwen 3.5 9B local LLM

---
+++ dist/assets/channel-DF0urBUc.js
@@ -0,0 +1,1 @@
+import{U as a,M as n}from"./index-CXgvyeVX.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ dist/assets/stateDiagram-v2-BHNVJYJU-C79u9QJf.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-AQP2D5EJ-jehzJEaA.js";import{_ as i}from"./index-CXgvyeVX.js";import"./chunk-55IACEB6-CiMSOpg6.js";import"./chunk-2J33WTMH-DTYO4A7W.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
