a97c865b84a441cb8b12c9c65c463d3879d86cd2
Govind Tank <govindtank600@gmail.com>
2026-07-29 11:19:38 +0530

    blog: Add WebGPU: Next-Generation Graphics and Compute for the Browser - 2026-07-29
    Automated via Qwen 3.5 9B local LLM

--- dist/assets/swimlanesDiagram-G3AALYLV-C7Wtnq7w.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U-BnVx3zNA.js";import{_ as a}from"./index-Y8OpAnWU.js";import"./chunk-5VM5RSS4-Cxw3v6Wm.js";import"./chunk-XXDRQBXY-BxcdkZS3.js";import"./chunk-VR4S4FIN-BQdwFWrs.js";import"./chunk-32BRIVSS-_lvPYmqR.js";import"./channel-Bc9az2ye.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
