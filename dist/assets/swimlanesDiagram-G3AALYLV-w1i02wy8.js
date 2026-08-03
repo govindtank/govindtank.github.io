c595fb14b29cf1cbd58f1cc895a82062a45d1eca
Govind Tank <govindtank600@gmail.com>
2026-08-03 14:16:23 +0530

    fix: attach unique IDs and scroll-mt-24 to headings for smooth Table of Contents navigation

--- dist/assets/swimlanesDiagram-G3AALYLV-w1i02wy8.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U-C9oZo8AG.js";import{_ as a}from"./index-3SoGZNZK.js";import"./chunk-5VM5RSS4-RMAT2HRr.js";import"./chunk-XXDRQBXY-BRO1RSWf.js";import"./chunk-VR4S4FIN-CO8zPAF8.js";import"./chunk-32BRIVSS-GuZTqkxV.js";import"./channel-Dh7sN_5e.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
