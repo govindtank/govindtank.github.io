6950c616143e45610084cfaa36beadf07a4e2b30
Govind Tank <govindtank600@gmail.com>
2026-08-13 12:21:48 +0530

    blog: Agentic AI Development: From Chat Assistants to Autonomous Coding Agents (war-story) - 2026-08-13
    Generated via Hermes blog pipeline v4

--- dist/assets/channel-lzHANmmH.js
+++
@@ -1,1 +0,0 @@
-import{U as a,D as n}from"./index-B3C_U_T_.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ dist/assets/swimlanesDiagram-G3AALYLV-LpdxFkkd.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-DGpp_PuQ.js";import{_ as a}from"./index-CD8u-rTu.js";import"./chunk-5VM5RSS4-Dh2M58Ox.js";import"./chunk-XXDRQBXY-C6hAz9UP.js";import"./chunk-VR4S4FIN-BHKXjQjO.js";import"./chunk-32BRIVSS-CMWr5V-Y.js";import"./channel-BZpSNEg8.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
