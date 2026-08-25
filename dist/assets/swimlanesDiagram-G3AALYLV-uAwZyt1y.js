beada7f2ed3c5c1e82aea9c2fa18e6c607058060
Govind Tank <govindtank600@gmail.com>
2026-08-25 18:35:34 +0530

    blog: AI-Native App Architecture: Designing Applications Around LLM Calls (roundup) - 2026-08-25
    Generated via Hermes blog pipeline v4

---
+++ dist/assets/swimlanesDiagram-G3AALYLV-uAwZyt1y.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-BYcql0Pf.js";import{_ as a}from"./index-wf7qWetY.js";import"./chunk-5VM5RSS4-Bs0S0pE8.js";import"./chunk-XXDRQBXY-BtF8VXdP.js";import"./chunk-VR4S4FIN-LzW40OTQ.js";import"./chunk-32BRIVSS-Db6OueDa.js";import"./channel-BcUyn0hJ.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
