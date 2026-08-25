c133aa5945087d39e8e2f465ad369cf9257ab0a3
Govind Tank <govindtank600@gmail.com>
2026-08-25 14:37:13 +0530

    blog: Kotlin 2.x and the K2 Compiler: What It Unlocked for Android Developers (roundup) - 2026-08-25
    Generated via Hermes blog pipeline v4

---
+++ dist/assets/swimlanesDiagram-G3AALYLV-S3iJ-7OW.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-BDb10gdG.js";import{_ as a}from"./index-C-wOstNy.js";import"./chunk-5VM5RSS4-KCfdqWbj.js";import"./chunk-XXDRQBXY-x0Dkv3oM.js";import"./chunk-VR4S4FIN-BUxovuHN.js";import"./chunk-32BRIVSS-wmGXG5k-.js";import"./channel-BbbMpDXG.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
